b7f49fd468dd5a7130c370878ea751e87b033e94
Allan McAleavy <Allan.McAleavy@gmail.com>
2016-02-05 17:26:42 +0000

    Update bitesize.c

--- bitesize.c
+++ bitesize.c
@@ -2,7 +2,7 @@
  * bitehist.c   Block I/O size histogram.
  *              For Linux, uses BCC, eBPF. See .py file.
  *
- * Copyright (c) 2015 Allan McAleavy
+ * Copyright (c) 2016 Allan McAleavy
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of version 2 of the GNU General Public
  * License as published by the Free Software Foundation.
